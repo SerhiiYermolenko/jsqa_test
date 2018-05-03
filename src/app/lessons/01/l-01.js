04a27af3e3e93b53df10c30967382560e52f9403
Serhii Yermolienko <serhii.yermolienko@synapse.com>
2018-05-03 11:27:06 +0300

    update

--- src/app/lessons/01/l-01.js
+++ src/app/lessons/01/l-01.js
@@ -382,7 +382,7 @@ console.log('\nLesson 01 - Homework:\n\n!!! Please answer the questions in this 
 
 // Task 01.01 - 1 point
 // Question: Can JavaScript file contain non-executable code?
-// Answer: No
+// Answer: Yes
 
 // Task 01.02 - 2 points
 // Question: How we can tell the JS interpreter to use strict mode?
